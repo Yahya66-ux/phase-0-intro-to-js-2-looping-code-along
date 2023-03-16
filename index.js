021373855d2a7b41e9055a44a2331efd1a3b2133
Yahya <yhmoha66@gmail.com>
2023-03-16 12:35:39 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(names, event){
+    let messages = [];
+    let thankYouNote = '';
+    for(let count = 0; count < names.length; count++){
+        thankYouNote = `Thank you, ${names[count]}, for the wonderful ${event} gift!`;
+        messages.push(thankYouNote);      
+    }
+   return messages;
+}
+
+function countDown(num){
+    for(let count = num; count >= 0; count--){
+        console.log(num);
+        num -= 1;
+
+    }
+}
+
+countDown(10);
